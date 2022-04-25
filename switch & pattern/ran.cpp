52af6a215232a5f6100a632d25aa0759aa5feeeb
Ajay Kumar Gupta <rg190559@gmail.com>
2022-04-25 13:21:42 +0530

    ran add

---
+++ switch & pattern/ran.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //int n=20;
+    //cout<<n<<endl;
+    for (int i =20; i >=2; i-=2)
+    {
+        //n-=2;
+        cout<<i<<endl;
+    }
+    // cout<<endl;
+}
